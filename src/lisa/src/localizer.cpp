77312823eb89f98cf68fa39f5e09fc5e1b0c5dbb
Peter P. Gengler <peter.gengler@jumpcloud.com>
2015-12-09 22:24:03 -0700

    Add simpler (turtlesim) localization for comparison.

--- src/lisa/src/localizer.cpp
+++ src/lisa/src/localizer.cpp
@@ -48,6 +48,8 @@ void initialposeCallback(const geometry_msgs::PoseWithCovarianceStamped& msg) {
   ROS_INFO("POSITION(initial): (%.3f,%.3f):%.3f", g_pose.pose.position.x, g_pose.pose.position.y, g_yaw);
 }
 
+void updatePoseFirstPass(double distance, double last_yaw, double yaw_delta);
+
 // TODO: Make a Localizer class to track state, see teleop_key.cpp for example.
 int main(int argc, char **argv) {
   ros::init(argc, argv, "localizer");
@@ -111,46 +113,13 @@ int main(int argc, char **argv) {
       float distance = WHEEL_ENCODER_M_DISTANCE_FROM_TICKS * float(wheel_encoder_delta);
       float yaw_delta = std::fmod(g_yaw - last_yaw, 2*PI);
 
-      // Figure out position deltas based on straight or banked travel
-      // TODO: Papers seem to indicate we can always do the straight estimate and call it good
-      // TODO: Alternatively look at sensor simulators and try that math?
-      float x_delta = 0.0;
-      float y_delta = 0.0;
-      if (yaw_delta == 0.0) {
-        //ROS_DEBUG("Straight motion");
-        x_delta = distance * cos(last_yaw);
-        y_delta = distance * sin(last_yaw);
-      } else {
-        //ROS_DEBUG("Banked motion");
-        // Get the turn radius from the distance and angle change
-        float turnRadius = distance / yaw_delta;
-        // Calculate x and y deltas as if we were at (0,0) pointed along x-axis
-        // NOTE: x is 'up' for us, so that's governed by Sin
-        float x_delta_origin = turnRadius * sin(yaw_delta);
-        float y_delta_origin = turnRadius * (-cos(yaw_delta) + 1.0);
+      // AVC localization
+      //updatePoseFirstPass(distance, last_yaw, yaw_delta);
 
-        // Now we need to rotate those deltas around (0,0) by our current orientation so they're correct
-        float sin_r = sin(last_yaw);
-        float cos_r = cos(last_yaw);
-        x_delta = x_delta_origin * cos_r - y_delta_origin * sin_r;
-        y_delta = x_delta_origin * sin_r + y_delta_origin * cos_r;
-      }
-
-      // Update g_pose and publish
-      g_pose.pose.position.x = g_pose.pose.position.x + x_delta;
-      g_pose.pose.position.y = g_pose.pose.position.y + y_delta;
+      // Simplified (Turtlesim)
+      g_pose.pose.position.x += std::sin(g_yaw) * distance;
+      g_pose.pose.position.y += std::cos(g_yaw) * distance;
       g_pose.pose.orientation = tf::createQuaternionMsgFromYaw(g_yaw);
-      ROS_DEBUG("QUATERNION: (%0.3f,%0.3f,%0.3f,%0.3f)",
-          g_pose.pose.orientation.x,
-          g_pose.pose.orientation.y,
-          g_pose.pose.orientation.z,
-          g_pose.pose.orientation.w
-          );
-      g_pose_pub.publish(g_pose);
-      ROS_DEBUG("POSITION: (%.3f,%.3f):%.3f",
-          g_pose.pose.position.x,
-          g_pose.pose.position.y,
-          g_yaw);
 
       // Update the path and publish
       g_path.points.push_back(g_pose.pose.position);
@@ -166,4 +135,47 @@ int main(int argc, char **argv) {
   }
 
   return 0;
+}
+
+void updatePoseFirstPass(double distance, double last_yaw, double yaw_delta) {
+  // Figure out position deltas based on straight or banked travel
+  // TODO: Papers seem to indicate we can always do the straight estimate and call it good
+  // TODO: Alternatively look at sensor simulators and try that math?
+  float x_delta = 0.0;
+  float y_delta = 0.0;
+  if (yaw_delta == 0.0) {
+    //ROS_DEBUG("Straight motion");
+    x_delta = distance * cos(last_yaw);
+    y_delta = distance * sin(last_yaw);
+  } else {
+    //ROS_DEBUG("Banked motion");
+    // Get the turn radius from the distance and angle change
+    float turnRadius = distance / yaw_delta;
+    // Calculate x and y deltas as if we were at (0,0) pointed along x-axis
+    // NOTE: x is 'up' for us, so that's governed by Sin
+    float x_delta_origin = turnRadius * sin(yaw_delta);
+    float y_delta_origin = turnRadius * (-cos(yaw_delta) + 1.0);
+
+    // Now we need to rotate those deltas around (0,0) by our current orientation so they're correct
+    float sin_r = sin(last_yaw);
+    float cos_r = cos(last_yaw);
+    x_delta = x_delta_origin * cos_r - y_delta_origin * sin_r;
+    y_delta = x_delta_origin * sin_r + y_delta_origin * cos_r;
+  }
+
+  // Update g_pose and publish
+  g_pose.pose.position.x += x_delta;
+  g_pose.pose.position.y += y_delta;
+  g_pose.pose.orientation = tf::createQuaternionMsgFromYaw(g_yaw);
+  ROS_DEBUG("QUATERNION: (%0.3f,%0.3f,%0.3f,%0.3f)",
+      g_pose.pose.orientation.x,
+      g_pose.pose.orientation.y,
+      g_pose.pose.orientation.z,
+      g_pose.pose.orientation.w
+      );
+  g_pose_pub.publish(g_pose);
+  ROS_DEBUG("POSITION: (%.3f,%.3f):%.3f",
+      g_pose.pose.position.x,
+      g_pose.pose.position.y,
+      g_yaw);
 }

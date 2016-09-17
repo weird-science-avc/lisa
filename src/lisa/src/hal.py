8e37b653c1b50334d72a536620bf4034076fdae4
Philip Deuchler <pbdeuchler@gmail.com>
2016-09-17 00:18:20 -0600

    Add button trigger for stop if running

--- src/lisa/src/hal.py
+++ src/lisa/src/hal.py
@@ -11,6 +11,7 @@ from Adafruit_BBIO import GPIO
 CLEAR_WAYPOINTS_RPC = "/waypoint_manager/clear"
 RESTART_WAYPOINTS_RPC = "/waypoint_manager/restart"
 START_NAV_RPC = "/navigator/start"
+STOP_NAV_RPC = "/navigator/stop"
 INITIAL_POSE_TOPIC = "initialpose"
 POSE_TOPIC = "/move_base_simple/goal"
 DEFAULT_COURSE_MAP_PARAMETER = "/hal/map_location"
@@ -58,6 +59,7 @@ def open_the_bay_doors():
     clear_waypoints = rospy.ServiceProxy(CLEAR_WAYPOINTS_RPC, std_srvs.srv.Empty)
     restart_waypoints = rospy.ServiceProxy(RESTART_WAYPOINTS_RPC, std_srvs.srv.Empty)
     start_navigation = rospy.ServiceProxy(START_NAV_RPC, std_srvs.srv.Empty)
+    stop_navigation = rospy.ServiceProxy(STOP_NAV_RPC, std_srvs.srv.Empty)
 
     rospy.loginfo("starting /hal")
     rospy.init_node(DEFAULT_NODE_NAME)
@@ -68,9 +70,11 @@ def open_the_bay_doors():
 
     setup_hardware()
     button_state = 0
+    running_state = 0
     while not rospy.is_shutdown():
         new_state = GPIO.input("P8_12")
-        if new_state == 1 and button_state == 0:
+        if new_state == 1 and button_state == 0 and running_state == 0:
+            running_state = 1
             print("CLEARING WAYPOINTS")
             clear_waypoints()
             print("PUBLISHING COURSE")
@@ -81,6 +85,9 @@ def open_the_bay_doors():
             set_initial_position(initial_position_topic)
             print("STARTING NAVIGATION...")
             start_navigation()
+        elif new_state == 1 and button_state == 0 and running_state == 1:
+            running_state = 0
+            stop_navigation()
         button_state = new_state
 
 if __name__ == '__main__':
